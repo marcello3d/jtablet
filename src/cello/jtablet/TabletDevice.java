6e7bad03ce45dd03b09e5ea4072b30f5d3272b95
Marcello Bastéa-Forte <marcello@cellosoft.com>
2009-04-26 08:30:34 +0000

    - major refactoring. now using cello.jtablet package for v2 code - refactored TabletEvent to extend MouseEvent and share a lot of the same code

---
+++ src/cello/jtablet/TabletDevice.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cello.jtablet;
+
+import java.io.Serializable;
+
+/**
+ * @author marcello
+ */
+public abstract class TabletDevice implements Serializable {
+
+
+	private TabletDevice() {}
+	
+	public static final TabletDevice STYLUS = new Stylus();
+	public static final TabletDevice STYLUS_ERASER = new StylusEraser();
+	public static final TabletDevice MOUSE = new Mouse();
+
+	public static class Stylus extends TabletDevice { }
+	public static class StylusEraser extends TabletDevice { }
+	public static class Mouse extends TabletDevice { }
+}
---
+++ src/cello/jtablet/TabletManager.java
@@ -0,0 +1,58 @@
+package cello.jtablet;
+
+import java.awt.Component;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import cello.jtablet.events.TabletListener;
+import cello.jtablet.impl.jpen.JPenTabletManager;
+
+/**
+ * Manages tablet implementations.
+ * 
+ * @author marcello
+ */
+public abstract class TabletManager {
+	
+	
+	/**
+	 * @param component
+	 * @return build a TabletManager for this system
+	 */
+	public static TabletManager getManager(Component component) {
+		return new JPenTabletManager(component);
+	}
+	
+	private static final Map<Component,TabletManager> managers = new WeakHashMap<Component, TabletManager>();
+
+	/**
+	 * Add a TabletListener to this Component
+	 * @param c 
+	 * @param l
+	 */
+	public static void addTabletListener(Component c, TabletListener l) {
+		synchronized (managers) {
+			TabletManager manager = managers.get(c);
+			if (manager == null) {
+				managers.put(c, manager = getManager(c));
+			}
+			manager.addTabletListener(l);
+		}
+	}
+	
+	/**
+	 * Remove a TabletListener from this Component
+	 * @param c 
+	 * @param l
+	 */
+	public static void removeTabletListener(Component c, TabletListener l) {
+		synchronized (managers) {
+			TabletManager manager = managers.get(c);
+			if (manager != null) {
+				manager.removeTabletListener(l);
+			}
+		}
+	}
+	protected abstract void addTabletListener(TabletListener l);
+	protected abstract void removeTabletListener(TabletListener l);
+}
