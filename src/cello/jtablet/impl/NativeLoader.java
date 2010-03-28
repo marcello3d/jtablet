f719bd7c4d8d8ce6d86d9c52111a4f55b403995d
Marcello Bastéa-Forte <marcello@cellosoft.com>
2010-03-28 07:16:47 +0000

    - added Mac OS X installer to build script - updated NativeLoader to just use the regular System.loadLibrary command

--- src/cello/jtablet/impl/NativeLoader.java
+++ src/cello/jtablet/impl/NativeLoader.java
@@ -1,13 +1,10 @@
 package cello.jtablet.impl;
 
-import java.io.File;
-
 /**
  * @author marcello
  */
 public class NativeLoader {
 	
-	private final static String JTABLET_FOLDER_NAME = ".jtablet/";
 	private static final String LIBNAME = "jtablet2";
 	
 	/**
@@ -15,27 +12,13 @@ public class NativeLoader {
 	/*package*/ NativeLoader() {
 	}
 
-	private String getLibraryPath(String homeFolder) throws NativeLoaderException {
-		File libraryDirectory = new File(homeFolder + "/" + JTABLET_FOLDER_NAME);
-		if (!libraryDirectory.exists()) {
-			throw new NativeLoaderException("Cannot find ~/" + JTABLET_FOLDER_NAME);
-		}
-		String libraryName = System.mapLibraryName(LIBNAME);
-		File nativeFile = new File(libraryDirectory, libraryName);
-		System.out.println("Attempting to load: "+nativeFile);
-		if (!nativeFile.exists()) {
-			throw new NativeLoaderException("Cannot find native library: ~/" + JTABLET_FOLDER_NAME + libraryName);
-		}
-		return nativeFile.getPath();
-	}
-
 	/*package*/ void load() throws NativeLoaderException {
-		String homeFolder = System.getProperty("user.home");
 		try {
-			System.load(getLibraryPath(homeFolder));
+			System.loadLibrary(LIBNAME);
 		} catch (Throwable t) {
 			// For security reasons, we don't want to leak an exception with the home folder name, so swap it out with ~
 			String exceptionString = t.toString();
+			String homeFolder = System.getProperty("user.home");
 			if (exceptionString.contains(homeFolder)) {
 				throw new NativeLoaderException(exceptionString.replace(homeFolder, "~"));
 			}
