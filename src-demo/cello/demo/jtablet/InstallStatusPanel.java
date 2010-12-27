61cc19676dd0ec907c6320634f0d82e1f8a66c87
Marcello Bastea-Forte <marcello@cellosoft.com>
2010-12-26 21:52:18 -0500

    more applet tweaks

--- src-demo/cello/demo/jtablet/InstallStatusPanel.java
+++ src-demo/cello/demo/jtablet/InstallStatusPanel.java
@@ -2,9 +2,14 @@ package cello.demo.jtablet;
 
 import cello.jtablet.DriverStatus;
 import cello.jtablet.TabletManager;
+import cello.jtablet.impl.NativeLoaderException;
 import cello.jtablet.installer.JTabletExtension;
 
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -29,24 +34,53 @@ public class InstallStatusPanel extends ClearPanel {
 		gbc.insets = new Insets(5,5,5,5);
 
         addLabel(gbc, "Installed version",
-                      JTabletExtension.getInstalledVersion());
+                        new JLabel(JTabletExtension.getInstalledVersion()));
         addLabel(gbc, "Install status (" + DemoApplet.REQUIRED_VERSION + ")",
-                      JTabletExtension.getInstallStatus(DemoApplet.REQUIRED_VERSION).toString());
+                        new JLabel(JTabletExtension.getInstallStatus(DemoApplet.REQUIRED_VERSION).toString()));
 
 		DriverStatus status = TabletManager.getDefaultManager().getDriverStatus();
+        addLabel(gbc, "Driver status",
+                        new JLabel(status.getState().toString()));
+
+
+        String exceptionString = "none";
 		Throwable throwable = status.getThrowable();
         if (throwable != null) {
             throwable.printStackTrace();
+            if (throwable instanceof NativeLoaderException) {
+                exceptionString = throwable.getMessage();
+                if (exceptionString.startsWith(UnsatisfiedLinkError.class.getName())) {
+                    if (exceptionString.contains("Can't find dependent libraries")) {
+                        exceptionString += "\n\nYour tablet's (WinTab) driver may not installed.";
+                    } else {
+                        exceptionString += "\n\nJTablet is incorrectly installed.";
+                    }
+                    exceptionString += "\n\n(The Java library loaded, but not the native driver).";
+                }
+            } else {
+                exceptionString = throwable.toString();
+            }
         }
-
-        addLabel(gbc, "Driver status", status.getState().toString());
-        addLabel(gbc, "Driver error", throwable == null ? "none" : throwable.toString());
+        addLabel(gbc, "Driver error", exceptionString);
 	}
 
     private void addLabel(GridBagConstraints gbc, String text, String value) {
-        JLabel valueLabel = new JLabel(value);
-        gbc.gridx=0;
-        gbc.anchor = GridBagConstraints.WEST;
+        if (value.contains("\n")) {
+            JTextArea textArea = new JTextArea(value);
+            textArea.setLineWrap(true);
+            textArea.setWrapStyleWord(true);
+            textArea.setEditable(false);
+            textArea.setBorder(null);
+            textArea.setOpaque(false);
+            textArea.setPreferredSize(new Dimension(300,300));
+            addLabel(gbc, text, textArea);
+        } else {
+            addLabel(gbc, text, new JLabel(value));
+        }
+    }
+    private void addLabel(GridBagConstraints gbc, String text, Component value) {
+        gbc.gridx = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         gbc.fill = GridBagConstraints.NONE;
         JLabel label = new JLabel(text+":",JLabel.LEFT);
         label.setFont(label.getFont().deriveFont(Font.BOLD));
@@ -54,7 +88,7 @@ public class InstallStatusPanel extends ClearPanel {
 
         gbc.gridx=2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        add(valueLabel, gbc);
+        add(value, gbc);
 
         gbc.gridy++;
     }
